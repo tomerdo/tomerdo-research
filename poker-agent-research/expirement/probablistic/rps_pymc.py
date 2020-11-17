dce88fa8574f7733e70c91149cab55685ed8daf4
Tomer Dobkin <tomer.dobkin@gmai.com>
2020-11-17 22:52:54 +0200

    computing average of many simulations

--- poker-agent-research/expirement/probablistic/rps_pymc.py
+++ poker-agent-research/expirement/probablistic/rps_pymc.py
@@ -16,10 +16,10 @@ def first_player_model(observed):
         posterior = pm.sample(step=pm.Metropolis(), return_inferencedata=True)
         posterior_pred = pm.sample_posterior_predictive(posterior)
 
-        print(az.summary(posterior))
-        print(posterior_pred)
+        # print(az.summary(posterior))
+        # print(posterior_pred)
         median_over_samples = np.median(posterior_pred['phi'], axis=0)
-        print(median_over_samples)
+        # print(median_over_samples)
         return median_over_samples
 
 
@@ -31,35 +31,43 @@ def second_player_model(num_of_samples=200):
         return phi.random(size=num_of_samples)
 
 
-def main():
-    # Learning phase
-    second_player_history = second_player_model(num_of_samples=200)
+def main(num_of_simulations=100):
+    total_first_player_wins = 0
+    total_second_player_wins = 0
+    total_ties = 0
+    for i in range(num_of_simulations):
+        # Learning phase
+        second_player_history = second_player_model(num_of_samples=200)
 
-    # gets a list of observed values and returns the distribution of probable action
-    first_player_next_moves = first_player_model(observed=second_player_history)
+        # gets a list of observed values and returns the distribution of probable action
+        first_player_next_moves = first_player_model(observed=second_player_history)
 
-    print(first_player_next_moves)
+        # print(first_player_next_moves)
 
-    first_player_next_moves = list(map(numerical_wins, first_player_next_moves))
-    print(first_player_next_moves)
+        first_player_next_moves = list(map(numerical_wins, first_player_next_moves))
+        # print(first_player_next_moves)
 
-    # Evaluation phase
-    second_player_next_moves = second_player_model(len(first_player_next_moves))
+        # Evaluation phase
+        second_player_next_moves = second_player_model(len(first_player_next_moves))
 
-    first_player_wins = 0
-    second_player_wins = 0
-    ties = 0
+        first_player_wins = 0
+        second_player_wins = 0
+        ties = 0
 
-    for i in range(len(second_player_next_moves)):
-        result = get_result(first_player_next_moves[i], second_player_next_moves[i])
-        if result > 0:
-            first_player_wins += 1
-        elif result < 0:
-            second_player_wins += 1
-        else:
-            ties += 1
-
-    print(f'wins: {first_player_wins}, loses: {second_player_wins}, ties: { ties}')
+        for j in range(len(second_player_next_moves)):
+            result = get_result(first_player_next_moves[j], second_player_next_moves[j])
+            if result > 0:
+                first_player_wins += 1
+            elif result < 0:
+                second_player_wins += 1
+            else:
+                ties += 1
+        total_first_player_wins += first_player_wins
+        total_second_player_wins += second_player_wins
+        total_ties += ties
+        print(f'in simulation {i}: wins: {first_player_wins}, loses: {second_player_wins}, ties: {ties}')
+    print(f'averages in all simulations is wins: {total_first_player_wins / num_of_simulations}'
+          f' loses:{total_second_player_wins / num_of_simulations} ties:{total_ties / num_of_simulations}')
 
 
 if __name__ == '__main__':
