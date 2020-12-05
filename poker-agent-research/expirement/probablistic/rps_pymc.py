c4f6c68c2dcbc3c93bd9dedb85d73fa62f89b468
Tomer Dobkin <tomer.dobkin@gmai.com>
2020-12-05 20:02:48 +0200

    refactoring the simulation, better methods, adding comments. one model for both players

--- poker-agent-research/expirement/probablistic/rps_pymc.py
+++ poker-agent-research/expirement/probablistic/rps_pymc.py
@@ -1,92 +1,82 @@
 import numpy as np
 import pymc3 as pm
-import arviz as az
-import matplotlib.pyplot as plt
-from time import time
-from probablistic.rps import RPS, get_result, numerical_wins
-
-model = None
+from probablistic.rps import get_result, numerical_wins
 
 
-def first_player_model(observed):
-    # t1 = time()
-    global model
-    model = pm.Model()
-    with model:
-        alpha = [1, 1, 1]
-        # observed = None
+def rps_player_model(alpha=[1, 1, 1], observed=None):
+    with pm.Model() as model:
         dirichlet = pm.Dirichlet('dirichlet', a=alpha)
         phi = pm.Categorical('phi', p=dirichlet, observed=observed)
         return model
 
 
-def infer():
-    global model
+def infer(model):
     with model:
-        posterior = pm.sample(step=pm.Metropolis(), model=model, return_inferencedata=True)
-        posterior_pred = pm.sample_posterior_predictive(posterior)
+        trace = pm.sample(step=pm.Metropolis(), model=model, return_inferencedata=True, progressbar=False)
+        return trace
 
-        # print(az.summary(posterior))
-        # print(posterior_pred)
+
+def sample_from_posterior(model, trace):
+    with model:
+        posterior_pred = pm.sample_posterior_predictive(trace, progressbar=False)
         median_over_samples = np.median(posterior_pred['phi'], axis=0)
-        # print(median_over_samples)
-        # t2 = time()
-        # elapsed = t2 - t1
-        # print('Elapsed time is %f seconds.' % elapsed)
         return median_over_samples
 
 
-def second_player_model(num_of_samples=5, alpha=[1, 5, 3]):
-    with pm.Model() as model_2:
-        dirichlet = pm.Dirichlet('dirichlet', a=alpha)
-        phi = pm.Categorical('phi', p=dirichlet)
-        return phi.random(size=num_of_samples)
+def sample_from_prior(model, num_of_samples):
+    with model:
+        samples = pm.sample_prior_predictive(num_of_samples)['phi']
+        return samples
 
 
 def simulate_with_latent_alpha(num_of_simulations=10, alpha=[1, 1, 1]):
-    total_first_player_wins = 0
-    total_second_player_wins = 0
+    total_smart_player_wins = 0
+    total_simple_player_wins = 0
     total_ties = 0
+
+    simple_player_observation = []
+
     for i in range(num_of_simulations):
         # Learning phase
-        second_player_history = second_player_model(num_of_samples=10, alpha=alpha)
+        simple_player = rps_player_model(alpha=alpha)
+
+        if len(simple_player_observation) == 0:
+            simple_player_observations = sample_from_prior(simple_player, num_of_samples=10)
 
         # gets a list of observed values and returns the distribution of probable action
-        first_player_model(observed=second_player_history)
-        first_player_next_moves = infer()
+        smart_player = rps_player_model(observed=simple_player_observations)
+        trace = infer(smart_player)
 
-        # print(first_player_next_moves)
-
-        first_player_next_moves = list(map(numerical_wins, first_player_next_moves))
-        # print(first_player_next_moves)
+        smart_player_next_moves = sample_from_posterior(smart_player, trace)
+        smart_player_next_moves = list(map(numerical_wins, smart_player_next_moves))
 
         # Evaluation phase
-        second_player_next_moves = second_player_model(len(first_player_next_moves), alpha=alpha)
+        simple_player_next_moves = sample_from_prior(simple_player, num_of_samples=10)
 
-        first_player_wins = 0
-        second_player_wins = 0
+        smart_player_wins = 0
+        simple_player_wins = 0
         ties = 0
 
-        for j in range(len(second_player_next_moves)):
-            result = get_result(first_player_next_moves[j], second_player_next_moves[j])
+        for j in range(len(simple_player_next_moves)):
+            result = get_result(smart_player_next_moves[j], simple_player_next_moves[j])
             if result > 0:
-                first_player_wins += 1
+                smart_player_wins += 1
             elif result < 0:
-                second_player_wins += 1
+                simple_player_wins += 1
             else:
                 ties += 1
-        total_first_player_wins += first_player_wins
-        total_second_player_wins += second_player_wins
+        total_smart_player_wins += smart_player_wins
+        total_simple_player_wins += simple_player_wins
         total_ties += ties
-        # print(f'in simulation {i}: wins: {first_player_wins}, loses: {second_player_wins}, ties: {ties}')
+        print(f'in simulation {i}: wins: {smart_player_wins}, loses: {simple_player_wins}, ties: {ties}')
     print(
-        f'For opponent\'s alpha vector: {alpha} averages in all simulations is wins: {total_first_player_wins / num_of_simulations} '
-        f' loses:{total_second_player_wins / num_of_simulations} ties:{total_ties / num_of_simulations}')
+        f'For opponent\'s alpha vector: {alpha} averages in all simulations is wins: {total_smart_player_wins / num_of_simulations} '
+        f' loses:{total_simple_player_wins / num_of_simulations} ties:{total_ties / num_of_simulations}')
 
 
 def main():
     simulate_with_latent_alpha(alpha=[1, 10, 10])
-    simulate_with_latent_alpha(alpha=[1, 3, 5])
+    # simulate_with_latent_alpha(alpha=[1, 3, 5])
     simulate_with_latent_alpha(alpha=[1, 1, 1])
     simulate_with_latent_alpha(alpha=[1, 6, 1])
     simulate_with_latent_alpha(alpha=[10, 6, 1])
